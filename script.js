07896710bbafb1e3ec95536375e0b860510a981f
Larry Gamboa <techmedia.lg@gmail.com>
2020-10-24 00:35:31 -0700

    fill in the blanks

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
 // List variables
+var timer;
+var time = 60;
+var score = 0;
+var questionIndex = 0;
 
 // Set up questions in array
+var questions = [
+    {
+        quizQuestion: "Question 1?",
+        choices: ["answer1", "answer2", "answer3", "answer4"],
+        answer: "answer1"
+    },
+    {
+        quizQuestion: "Question 2?",
+        choices: ["answer1", "answer2", "answer3", "answer4"],
+        answer: "answer1"
+    },
+    {
+        quizQuestion: "Question 3?",
+        choices: ["answer1", "answer2", "answer3", "answer4"],
+        answer: "answer1"
+    },
+    {
+        quizQuestion: "Question 4?",
+        choices: ["answer1", "answer2", "answer3", "answer4"],
+        answer: "answer1"
+    },
+    {
+        quizQuestion: "Question 5?",
+        choices: ["answer1", "answer2", "answer3", "answer4"],
+        answer: "answer1"
+    }
+];
 
 // Start the quiz using a function
+function startQuiz() {
+
+};
 
 // Question markup using a function
+function displayQuestion() {
+
+};
 
 // Process the answers
+function processAnswer() {
+
+};
 
 // End the quiz
+function endQuiz() {
+
+};
 
